2f95fc5c3f6f9f02d1aaf3c207033ebaf51d99e8
Miguel Angel Lopez Hermida <lacarricanta@hotmail.com>
2026-01-18 11:05:01 +0000

    Commit 18-01-2026

--- src/pages/Detalle.jsx
+++ src/pages/Detalle.jsx
@@ -5,29 +5,23 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Detalle = () => {
 
-    const { store, dispatch } = useGlobalReducer()
-    const { uid } = useParams()
+  const { store, dispatch } = useGlobalReducer()
+  const { uid } = useParams()
 
-    
-  const [detallesPersonajes, setDetallesPersonajes] = useState({
-    name: '',
-    birth_year: '',
-    homeworld: '',
-    species: '',
-    films: ''
-  });
+
+  const [detallesPersonajes, setDetallesPersonajes] = useState({});
 
   //  CARGAR DATOS 
   useEffect(() => {
-fetch(`https://www.swapi.tech/api/people/${uid}`)
-            .then(res => res.json())
-            .then(data => {
-                // SWAPI.tech guarda los datos en data.result.properties
-                if (data.result) {
-                    setDetallesPersonajes(data.result.properties);
-                }
-            })
-            .catch(err => console.error("Error:", err));
+    fetch(`https://www.swapi.tech/api/people/${uid}`)
+      .then(res => res.json())
+      .then(data => {
+        // SWAPI.tech guarda los datos en data.result.properties
+        if (data.result) {
+          setDetallesPersonajes(data.result.properties);
+        }
+      })
+      .catch(err => console.error("Error:", err));
 
     if (uid && store.personajes.length > 0) {
       const detalleDePersonaje = store.personajes.find(personaje => personaje.uid === Number(uid));
@@ -36,34 +30,56 @@ fetch(`https://www.swapi.tech/api/people/${uid}`)
       }
     }
   }, [uid]);
+  const generarDescripcion = (personaje) => {
+    // 1. Extraemos las propiedades
+    const { name, birth_year, gender, height, mass, hair_color, eye_color } = personaje;
+
+    // 2. Traducción simple de términos técnicos
+    const tradGenero = gender === "male" ? "a man" : gender === "female" ? "a woman" : "an individual";
+
+    // 3. Construcción del string dinámico
+    return `${name} is ${tradGenero} born in the year ${birth_year}.
+    Physically, they stand out with a height of ${height} cm and a weight of ${mass} kg, featuring distinctive ${eye_color} eyes.`
+  };
 
 
-    return (
-        <div className="container bg-dark ">
-            <div className="card">
-                <div className='row'>
-                    <div className='imagen col-12 col-md-6 col-lg-5'>
-                    <img src="https://lumiere-a.akamaihd.net/v1/images/sw-soulmate-leia-result-quiz-and-poll-images_04527594.jpeg?region=56%2C0%2C889%2C500" className="card-img-top" alt="..." />
-                </div>
-                    <div className="card-body col-12 col-md-6 col-lg-7">
-                    <h5 className="card-title"> {detallesPersonajes.name}</h5>
-                    <p className="card-text">
-                       <p>Año de nacimiento: {detallesPersonajes.birth_year}
-                        </p>
-                        <p>
-                            Planeta habitual: {detallesPersonajes.homeworld} 
-                        </p>
-                      <p>
-                         Especie: "{detallesPersonajes.species}"
-                      </p>
-                      <p>
-                        Películas en las que aparece: {detallesPersonajes.films}
-                      </p>
-                        </p>
-                    <Link to="/" className="btn btn-primary">Inicio</Link>
-                </div>
-                </div>
+  return (
+    <div className="container bg-dark ">
+      <div className="card h-100 text-white bg-dark">
+        <div className='row'>
+          <div className='imagen col-12 col-md-6 col-lg-6 p-2'>
+            <img src="https://lumiere-a.akamaihd.net/v1/images/sw-soulmate-leia-result-quiz-and-poll-images_04527594.jpeg?region=56%2C0%2C889%2C500" className="card-img-top" alt="..." />
+          </div>
+          <div className="card-body col-12 col-md-6 col-lg-6 d-flex row">
+            <h3 className="nombre-detalle card-title fw-bold fs-3"> {detallesPersonajes.name}</h3>
+            <p className="card-text text-white-50">
+              {generarDescripcion(detallesPersonajes)}
+            </p>
+            <div className='button d-flex justify-content-center'>
+              <Link to="/" className="btn btn-sm w-100 p-0 mt-auto btn-primary btn-outline-danger btn-info">Back to home</Link>
             </div>
+          </div>
+          <div className=' row mt-3 py-3 g'>
+            <div className="col-md-3 col-6" style={{ overflowX: 'auto' }}>
+              <h6 className=''>Films:</h6>
+              <p className='text-break text-secondary text-truncate'>{detallesPersonajes.films}</p>
+            </div>
+            <div className="col-md-3 col-6" style={{ overflowX: 'auto' }}>
+              <h6 className=''>Homeworld:</h6>
+              <p className='text-break text-secondary text-truncate'>{detallesPersonajes.homeworld}</p>
+            </div>
+            
+             <div className="col-md-3 col-6" style={{ overflowX: 'auto' }}>
+              <h6 className=''>Created:</h6>
+              <p className='text-break text-secondary text-truncate'>{detallesPersonajes.created}</p>
+            </div>
+             <div className="col-md-3 col-6" style={{ overflowX: 'auto' }}>
+              <h6 className=''>Edited:</h6>
+              <p className='text-break text-secondary text-truncate'>{detallesPersonajes.edited}</p>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,16 +26,16 @@ export const Home = () => {
 
 	return (
 		<div className="text-center bg-dark">
-			<h1>Personajes</h1>
-			<div className="text-center mt-5 mx-3 d-flex row g-3" >
+			<h1 className="titulo p-3 text-start">Characters</h1>
+			<div className="text-center mx-3 d-flex row g-4" >
 				{store.personajes.map((item, index) =>
-					<div className=" col-12 col-sm-6 col-md-3 col-lg-2" key={index} >
-						<div className="card">
+					<div className=" col-12 col-sm-6 col-md-4 col-lg-2" key={index} >
+						<div className="card h-100 bg-secondary text-white mb-3">
 							<img src="https://lumiere-a.akamaihd.net/v1/images/sw-soulmate-leia-result-quiz-and-poll-images_04527594.jpeg?region=56%2C0%2C889%2C500" className="card-img-top" alt="..." />
-							<div className="card-body">
-								<h5 className="card-title"> {item.name}</h5>
-								<p className="card-text">Some quick example text to build on the card title</p>
-								<Link to={`/detalle/${item.uid}`}className="btn btn-primary">Detalles</Link>
+							<div className="card-body d-flex flex-column text-center align-items-center">
+								<h6 className="card-title"> {item.name}</h6>
+								<Link to={`/detalle/${item.uid}`}
+								className="btn btn-sm w-50 p-0 mt-auto btn-primary btn-outline-danger btn-info">Details</Link>
 							</div>
 						</div>
 
